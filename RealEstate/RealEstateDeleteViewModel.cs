d89e78183077ab7983466efd5f07080c7cbbb367
markwiz <mmark.kuusik@gmail.com>
2025-10-05 18:56:11 +0300

    RealEstate Models

---
+++ RealEstate/RealEstateDeleteViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ShopTARge24.Models.RealEstate
+{
+    public class RealEstateDeleteViewModel
+    {
+        public Guid? Id { get; set; }
+        public double? Area { get; set; }
+        public string? Location { get; set; }
+        public int? RoomNumber { get; set; }
+        public string? BuildingType { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+    }
+}
---
+++ RealEstate/RealEstateDetailsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ShopTARge24.Models.RealEstate
+{
+    public class RealEstateDetailsViewModel
+    {
+        public Guid? Id { get; set; }
+        public double? Area { get; set; }
+        public string? Location { get; set; }
+        public int? RoomNumber { get; set; }
+        public string? BuildingType { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+    }
+}
